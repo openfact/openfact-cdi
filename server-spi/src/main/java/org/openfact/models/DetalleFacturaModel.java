f23401cae1f7490d65d6c5306183e5232246fc9a
Alex Pariona <lx7pary@gmail.com>
2016-07-01 11:24:08 -0500

    .

--- server-spi/src/main/java/org/openfact/models/DetalleFacturaModel.java
+++ server-spi/src/main/java/org/openfact/models/DetalleFacturaModel.java
@@ -3,7 +3,7 @@ package org.openfact.models;
 /**
  * Created by Alex Pariona on 1/07/2016.
  */
-public class DetalleFacturaModel extends DetalleBoletaModel {
+public class DetalleFacturaModel {
 	FacturaModel factura;
 	String UnidadMedida;
 	double Cantidad;
