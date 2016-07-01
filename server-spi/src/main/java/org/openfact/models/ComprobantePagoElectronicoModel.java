bc3187a801273e718066b3cc068d831f0ab4f501
Alex Pariona <lx7pary@gmail.com>
2016-07-01 11:22:15 -0500

    CREACION DE CLASES COMPROBANTES DE PAGO ELECTRONICOS Y SUS DETALLES

--- server-spi/src/main/java/org/openfact/models/ComprobantePagoElectronicoModel.java
+++ server-spi/src/main/java/org/openfact/models/ComprobantePagoElectronicoModel.java
@@ -1,7 +1,39 @@
 package org.openfact.models;
 
+import java.util.Date;
+
 /**
- * Created by ahren-dev01 on 1/07/2016.
+ * Created by Alex Pariona on 1/07/2016.
  */
 public class ComprobantePagoElectronicoModel {
+	Date FechaEmision;
+	/*-----INI DATOS CONTRIBUYENTE----*/
+	String RazonSocial;
+	String NombreComercial;
+	String DomicilioFiscal;
+	String NumeroRuc;
+	/*-----END DATOS CONTRIBUYENTE----*/
+	String TipoDocumento;/* 01--caso de facturas *//* 06--Caso de Boleta */
+	String SerieComprobantePago;
+	String NumeroComprobantePago;
+	String TipoNumeroDocumento;
+	String NumeroDocumento;
+	/*------------RESUMEN IMPORTE TOTAL----------*/
+	double TotalVentaGravadas;
+	double TotalVentaInafectas;
+	double TotalVentaExoneradas;
+	double TotalIgv;
+	double TotalISC;
+	double TotalTributos;
+	double TotalOtrosCargos;
+	double TotalDescuentos;
+	double TotalVenta;
+	String TipoMoneda;
+	/*------------INFORMACION ADICIONAL----------*/
+	String TipoGuia;
+	String NumeroGuia;
+	String LeyendaTransferencia;
+	String LeyendaRepresentacion;
+	String LeyendaAutorizacion;
+	double TotalVentaGratuita;
 }
---
+++ server-spi/src/main/java/org/openfact/models/DetalleFacturaModel.java
@@ -0,0 +1,17 @@
+package org.openfact.models;
+
+/**
+ * Created by Alex Pariona on 1/07/2016.
+ */
+public class DetalleFacturaModel extends DetalleBoletaModel {
+	FacturaModel factura;
+	String UnidadMedida;
+	double Cantidad;
+	String Descripcion;
+	double PrecioVenta;
+	// double Igv;
+	double ValorUnitario;
+	String Codigo;// 01--pv+igv/02--operaciones no onerosas
+	String AfectacionIgv;
+	String ISC;// Catalogo 8
+}
