e499a9d0d0512d00e88463c0493867a2893f6b01
carlosthe19916 <carlosthe19916@gmail.com>
2016-07-01 08:49:32 -0500

    basic models

--- server-spi/src/main/java/org/openfact/models/DocumentoReporteElectronicoModel.java
+++ server-spi/src/main/java/org/openfact/models/DocumentoReporteElectronicoModel.java
@@ -3,5 +3,5 @@ package org.openfact.models;
 /**
  * Created by ahren-dev01 on 1/07/2016.
  */
-public class RetraccionModel {
+public class DocumentoReporteElectronicoModel {
 }
---
+++ server-spi/src/main/java/org/openfact/models/enums/EstadoEnvio.java
@@ -0,0 +1,8 @@
+package org.openfact.models.enums;
+
+/**
+ * Created by ahren-dev01 on 1/07/2016.
+ */
+public enum EstadoEnvio {
+    ENVIADO, APROBADO;
+}
