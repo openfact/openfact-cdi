10c699b38a980b6bbe69a0693532a974eab1546c
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2016-07-06 16:47:26 -0500

    ADD TIPO dOCUMENTO TYOE

--- server-spi/src/main/java/org/openfact/models/enums/TipoDocumentoType.java
+++ server-spi/src/main/java/org/openfact/models/enums/TipoDocumentoType.java
@@ -2,6 +2,24 @@ package org.openfact.models.enums;
 
 public enum TipoDocumentoType {
 
-    BASE, OTRO;
+    FACTURA("FACTURA", "01"),
+    BOLETA("BOLETA DE VENTA", "02"),
+    NOTA_CREDITO("NOTA DE CREDITO", "03");
+
+    private final String descripcion;
+    private final String codigo;
+
+    TipoDocumentoType(String descripcion, String codigo) {
+        this.descripcion = descripcion;
+        this.codigo = codigo;
+    }
+
+    public String getDenominacion() {
+        return this.descripcion;
+    }
+
+    public String getCodigo() {
+        return this.codigo;
+    }
 
 }
