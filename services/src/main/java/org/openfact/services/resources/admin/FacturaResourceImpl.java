3f02e21007ef1bd884e11a384cd419393170f3f7
Alex Pariona <lx7pary@gmail.com>
2016-07-07 16:45:04 -0500

    OF-94

--- services/src/main/java/org/openfact/services/resources/admin/FacturaResourceImpl.java
+++ services/src/main/java/org/openfact/services/resources/admin/FacturaResourceImpl.java
@@ -1,7 +1,110 @@
 package org.openfact.services.resources.admin;
 
+import org.openfact.models.FacturaProvider;
+import org.openfact.representations.idm.EventoRepresentation;
+import org.openfact.representations.idm.FacturaRepresentation;
+import org.openfact.services.managers.FacturaManager;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
 /**
  * Created by AHREN on 06/07/2016.
  */
-public class FacturaResourceImpl {
+@Stateless
+public class FacturaResourceImpl implements  FacturaResource {
+
+    @PathParam("idFactura")
+    private String idFactura;
+
+    @Inject
+    private FacturaManager facturaManager;
+
+    @Inject
+    private FacturaProvider facturaProvider;
+
+    @Inject
+    private DetalleFacturasResource  detalleFacturasResource;
+
+
+    @Override public FacturaRepresentation toRepresentation() {
+        return null;
+    }
+
+    @Override public void update(FacturaRepresentation facturaRepresentation) {
+
+    }
+
+    @Override public Response send() {
+        return null;
+    }
+
+    @Override public Response status() {
+        return null;
+    }
+
+    @Override public List<EventoRepresentation> eventos() {
+        return null;
+    }
+
+    @Override public Response downloadXml() {
+        return null;
+    }
+
+    @Override public Response downloadXls() {
+        return null;
+    }
+
+    @Override public Response downloadPdf() {
+        return null;
+    }
+
+    @Override public Response downloadCdr() {
+        return null;
+    }
 }
+
+/*
+* @PathParam("idProyecto")
+	private String idProyecto;
+
+	@Inject
+	private ProyectoManager proyectoManager;
+
+	@Inject
+	private ProyectoProvider proyectoProvider;
+
+	@Inject
+	private HistorialesProyectoResource historialProyectoResource;
+
+	private ProyectoModel getProyectoModel() {
+		return proyectoProvider.findById(idProyecto);
+	}
+
+	@RolesAllowed(value = { Roles.ver_proyectos })
+	@Override
+	public ProyectoRepresentation toRepresentation() {
+		ProyectoRepresentation rep = ModelToRepresentation.toRepresentation(getProyectoModel());
+		if (rep != null) {
+			return rep;
+		} else {
+			throw new NotFoundException("Proyecto no encontrado");
+		}
+	}
+
+	@RolesAllowed(value = { Roles.administrar_proyectos, Roles.administrar_proyectos_direccionRegional })
+	@Override
+	public void update(ProyectoRepresentation rep) {
+		proyectoManager.update(getProyectoModel(), rep);
+	}
+
+	@PermitAll
+	@Override
+	public HistorialesProyectoResource historiales() {
+		return historialProyectoResource;
+	}
+*
+* */
