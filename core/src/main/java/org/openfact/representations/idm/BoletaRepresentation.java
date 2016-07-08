f0dbc8835a0d849de0a376a643dceea73dd5eef6
Alex Pariona <lx7pary@gmail.com>
2016-07-08 10:03:01 -0500

    OF-94

--- core/src/main/java/org/openfact/representations/idm/BoletaRepresentation.java
+++ core/src/main/java/org/openfact/representations/idm/BoletaRepresentation.java
@@ -1,11 +1,29 @@
 package org.openfact.representations.idm;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by Gissela-Sistemas on 8/07/2016.
  */
 public class BoletaRepresentation extends ComprobantePagoRepresentation implements Serializable {
 
+    public String getNumeroDocumentoRelacionado() {
+        return numeroDocumentoRelacionado;
+    }
+
+    public void setNumeroDocumentoRelacionado(String numeroDocumentoRelacionado) {
+        this.numeroDocumentoRelacionado = numeroDocumentoRelacionado;
+    }
+
+    public List<DetalleBoletaRepresentation> getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(List<DetalleBoletaRepresentation> detalle) {
+        this.detalle = detalle;
+    }
+
     private String numeroDocumentoRelacionado;
+    List<DetalleBoletaRepresentation> detalle;
 }
