b3cd85fc8b34d99c4d1e1048cfb3d1f2d024cfc4
Alex Pariona <lx7pary@gmail.com>
2016-07-11 17:28:49 -0500

    Signed-off-by: Alex Pariona <lx7pary@gmail.com>

--- server-spi/src/main/java/org/openfact/models/utils/RepresentationToModel.java
+++ server-spi/src/main/java/org/openfact/models/utils/RepresentationToModel.java
@@ -10,23 +10,25 @@ import org.openfact.models.*;
 import org.openfact.representations.idm.EmisorRepresentation;
 import org.openfact.representations.idm.FacturaRepresentation;
 
-@Stateless @TransactionAttribute(TransactionAttributeType.REQUIRED) public class RepresentationToModel {
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class RepresentationToModel {
 
     public EmisorModel createEmisor(EmisorRepresentation rep, EmisorProvider provider) {
         EmisorModel model = provider.create(rep.getRuc(), rep.getRazonSocial());
-        HistorialEmisorModel historialEmisorModel = provider
-                .createHistorial(model, rep.getResolucionAutorizacion(),
-                        rep.getMensajeRepresentacionImpresa());
+        HistorialEmisorModel historialEmisorModel = provider.createHistorial(model,
+                rep.getResolucionAutorizacion(), rep.getMensajeRepresentacionImpresa());
         historialEmisorModel.setMensajeServicioGratuito(rep.getMensajeServicioGratuito());
         model.setNombreComercial(rep.getNombreComercial());
         model.setDomicilioFiscal(rep.getDomicilioFiscal());
         model.commit();
         return model;
     }
+
     public HistorialEmisorModel createHistorial(EmisorRepresentation rep, EmisorProvider provider) {
-        EmisorModel model=provider.findById(rep.getId());
-        HistorialEmisorModel historialEmisorModel = provider.createHistorial(model, rep.getResolucionAutorizacion(),
-                        rep.getMensajeRepresentacionImpresa());
+        EmisorModel model = provider.findById(rep.getId());
+        HistorialEmisorModel historialEmisorModel = provider.createHistorial(model,
+                rep.getResolucionAutorizacion(), rep.getMensajeRepresentacionImpresa());
         historialEmisorModel.setMensajeServicioGratuito(rep.getMensajeServicioGratuito());
         historialEmisorModel.commit();
         return historialEmisorModel;
@@ -35,11 +37,11 @@ import org.openfact.representations.idm.FacturaRepresentation;
     public FacturaModel createFactura(FacturaRepresentation rep, EmisorModel emisor,
             FacturaProvider provider) {
         AdquirienteModel adquiriente = null;
-        //provider.createAdquiriente(numeroDocumento);
+        // provider.createAdquiriente(numeroDocumento);
 
         // Crear numeracion
-        NumeracionComprobantePagoModel numeracion = provider
-                .createNumeracion(rep.getSerie(), rep.getNumero());
+        NumeracionComprobantePagoModel numeracion = provider.createNumeracion(rep.getSerie(),
+                rep.getNumero());
 
         // Create resumen
         ResumenComprobantePagoModel resumen;
@@ -68,46 +70,50 @@ import org.openfact.representations.idm.FacturaRepresentation;
         resumen.setIsc(rep.getTotalIsc());
 
         FacturaModel model = provider.create(emisor, adquiriente, rep.getFechaEmision(), numeracion, resumen);
-        //model.setTipoDocumentoRelacionadoType(type);
-        //model.setInformacionAdicional(informacionAdicional);
+        // model.setTipoDocumentoRelacionadoType(type);
+        // model.setInformacionAdicional(informacionAdicional);
         model.commit();
         return model;
     }
 
-	public BoletaModel createBoleta(BoletaRepresentation rep, EmisorModel emisor,BoletaProvider provider){
-		AdquirienteModel adquiriente = null;
+    public BoletaModel createBoleta(BoletaRepresentation rep, EmisorModel emisor, BoletaProvider provider) {
+        AdquirienteModel adquiriente = null;
 
-         //crear numeracion boleta
-		NumeracionComprobantePagoModel numeracion =provider.createNumeracion(rep.getSerie(), rep.getNumero());
+        // crear numeracion boleta
+        NumeracionComprobantePagoModel numeracion = provider.createNumeracion(rep.getSerie(),
+                rep.getNumero());
 
-		//crear resumen boleta
-		ResumenComprobantePagoModel resumen;
-		if (rep.getTotalOperacionesExoneradas()!=null){
+        // crear resumen boleta
+        ResumenComprobantePagoModel resumen;
+        if (rep.getTotalOperacionesExoneradas() != null) {
 
-			resumen =provider.createResumenExonerado(rep.getTotalOperacionesExoneradas(),rep.getMoneda(),rep.getImporteTotal());
-			resumen.setTotalGravado(rep.getTotalOperacionesGravadas());
-			resumen.setTotalInafecto(rep.getTotalOperacionesInafectas());
-		}else if (rep.getTotalOperacionesGravadas()!=null){
-			resumen = provider.createResumenGravado(rep.getTotalOperacionesGravadas(),rep.getMoneda(), rep.getImporteTotal());
-			resumen.setTotalExonerado(rep.getTotalOperacionesGravadas());
-			resumen.setTotalInafecto(rep.getTotalOperacionesInafectas());
-		}else if (rep.getTotalOperacionesInafectas()!=null) {
-			resumen = provider.createResumenInafecto(rep.getTotalOperacionesInafectas(), rep.getMoneda(), rep.getImporteTotal());
-			resumen.setTotalGravado(rep.getTotalOperacionesGravadas());
-			resumen.setTotalExonerado(rep.getTotalOperacionesInafectas());
-		}else {
-			throw new ModelException("La boleta no tiene operaciones gravadas, inafectas y/o exoneradas");
-		}
-			resumen.setCargos(rep.getCargos());
-		resumen.setDescuentos(rep.getDescuentos());
-		resumen.setTributos(rep.getTributos());
-		resumen.setIgv(rep.getTotalIgv());
-		resumen.setIsc(rep.getTotalIsc());
+            resumen = provider.createResumenExonerado(rep.getTotalOperacionesExoneradas(), rep.getMoneda(),
+                    rep.getImporteTotal());
+            resumen.setTotalGravado(rep.getTotalOperacionesGravadas());
+            resumen.setTotalInafecto(rep.getTotalOperacionesInafectas());
+        } else if (rep.getTotalOperacionesGravadas() != null) {
+            resumen = provider.createResumenGravado(rep.getTotalOperacionesGravadas(), rep.getMoneda(),
+                    rep.getImporteTotal());
+            resumen.setTotalExonerado(rep.getTotalOperacionesGravadas());
+            resumen.setTotalInafecto(rep.getTotalOperacionesInafectas());
+        } else if (rep.getTotalOperacionesInafectas() != null) {
+            resumen = provider.createResumenInafecto(rep.getTotalOperacionesInafectas(), rep.getMoneda(),
+                    rep.getImporteTotal());
+            resumen.setTotalGravado(rep.getTotalOperacionesGravadas());
+            resumen.setTotalExonerado(rep.getTotalOperacionesInafectas());
+        } else {
+            throw new ModelException("La boleta no tiene operaciones gravadas, inafectas y/o exoneradas");
+        }
+        resumen.setCargos(rep.getCargos());
+        resumen.setDescuentos(rep.getDescuentos());
+        resumen.setTributos(rep.getTributos());
+        resumen.setIgv(rep.getTotalIgv());
+        resumen.setIsc(rep.getTotalIsc());
 
-		BoletaModel model= provider.create(emisor, adquiriente, rep.getFechaEmision(), numeracion,resumen);
-		model.commit();
-		return model;
+        BoletaModel model = provider.create(emisor, adquiriente, rep.getFechaEmision(), numeracion, resumen);
+        model.commit();
+        return model;
 
-	}
+    }
 
 }
