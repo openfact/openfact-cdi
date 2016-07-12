21ef62fd6139785fc0e4cf559261aab9b3c97fab
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2016-07-12 12:01:20 -0500

    added testsuite package

---
+++ testsuite/wildfly-swarm/src/main/java/org/openfact/test/MyTest.java
@@ -0,0 +1,20 @@
+package org.openfact.test;
+
+/*import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.runner.RunWith;*/
+
+//@RunWith(Arquillian.class)
+public class MyTest {
+
+    //@Deployment
+    /*public static Archive createDeployment() {
+        //JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+        // deployment.addResource(MyResource.class );
+        //return deployment;
+        return null;
+    }*/
+
+}
---
+++ wildfly-swarm/src/main/java/org/openfact/swarm/Main.java
@@ -0,0 +1,18 @@
+package org.openfact.swarm;
+
+/*import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;*/
+
+public class Main {
+
+    /*public static void main(String... args) throws Exception {
+        Swarm container = new Swarm();
+
+        JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
+        //deployment.addClass(HelloRest.class);
+        deployment.addAllDependencies();
+        container.start().deploy(deployment);
+    }*/
+}
