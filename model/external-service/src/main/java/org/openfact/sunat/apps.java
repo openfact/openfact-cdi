8b7b70c520d68dee9531ae906484db62c0bf024b
Alex Pariona <lx7pary@gmail.com>
2016-07-23 09:10:53 -0500

    Firmado y validacion de las firmas de los xml Generados, creacion de test de prueba para todos

---
+++ model/external-service/src/main/java/org/openfact/sunat/apps.java
@@ -0,0 +1,7 @@
+package org.openfact.sunat;
+
+/**
+ * Created by Alex Pariona-"alexpariona@openfact.com" on 22/07/2016.
+ */
+public class apps {
+}
--- model/storage/src/main/java/org/openfact/models/ubl/package-info.java
+++ model/storage/src/main/java/org/openfact/models/ubl/package-info.java
@@ -4,26 +4,7 @@
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
 // Generado el: 2016.07.20 a las 12:44:20 PM COT 
 //
-/*
-@XmlSchema(
-        xmlns = {
-                @XmlNs(prefix = "xmlnsRetention", namespaceURI = "urn:sunat:names:specification:ubl:peru:schema:xsd:Retention-1"),
-                @XmlNs(prefix = "xmlnsInvoice", namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:UblGenerator-2"),
-                @XmlNs(prefix = "xmlnsCreditNote", namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2"),
-                @XmlNs(prefix = "xmlnsDebitNote", namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:DebitNote-2"),
-                @XmlNs(prefix = "xmlnsVoidedDocuments", namespaceURI = "urn:sunat:names:specification:ubl:peru:schema:xsd:VoidedDocuments-1"),
-                @XmlNs(prefix = "xmlnsSummaryDocuments", namespaceURI = "urn:sunat:names:specification:ubl:peru:schema:xsd:SummaryDocuments-1"),
-                @XmlNs(prefix = "sac", namespaceURI = "urn:sunat:names:specification:ubl:peru:schema:xsd:SunatAggregateComponents-1"),
-                @XmlNs(prefix = "cac", namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"),
-                @XmlNs(prefix = "cbc", namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"),
-                @XmlNs(prefix = "udt", namespaceURI = "urn:un:unece:uncefact:data:specification:UnqualifiedDataTypesSchemaModule:2"),
-                @XmlNs(prefix = "ccts", namespaceURI = "urn:un:unece:uncefact:documentation:2"),
-                @XmlNs(prefix = "ext", namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2"),
-                @XmlNs(prefix = "qdt", namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2"),
-                @XmlNs(prefix = "ds", namespaceURI = "http://www.w3.org/2000/09/xmldsig#"),
-                @XmlNs(prefix = "xsi", namespaceURI = "http://www.w3.org/2001/XMLSchema-instance")
-        }
-)*/
+
 @javax.xml.bind.annotation.XmlSchema(namespace = "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
         xmlns = {
                 @XmlNs(namespaceURI = "urn:sunat:names:specification:ubl:peru:schema:xsd:SunatAggregateComponents-1", prefix = "sac"),
@@ -34,7 +15,8 @@
                 @XmlNs(namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2", prefix = "ext"),
                 @XmlNs(namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2", prefix = "qdt"),
                 @XmlNs(namespaceURI = "http://www.w3.org/2000/09/xmldsig#", prefix = "ds"),
-                @XmlNs(namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2", prefix = "")
+                @XmlNs(namespaceURI = "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2", prefix = ""),
+                @XmlNs(namespaceURI = "http://www.w3.org/2001/XMLSchema-instance", prefix = "xsi")
 
         }
 )
